a4dcb3c771d3de8e7bde661098867f5348bc444d
primp33 <prim.poshyanonda@student.uts.edu.au>
2023-03-22 19:39:10 +0700

    Levels completed

--- Lab4 Game Design Studio Autumn 2023/Assets/Scripts/Levels.cs
+++ Lab4 Game Design Studio Autumn 2023/Assets/Scripts/Levels.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,12 +13,13 @@ public class Levels : MonoBehaviour
     private GameObject win;
     private GameObject lose;
     public Text Ammo;
-    public Text Time;
+    public Text timeText;
     public Text losing;
     public Text winning;
-    public GameObject life1, life2, life3;
+    public GameObject[] lives;
     private int life = 3;
-    private int time = 120;
+    private List<float> timeleft = new List<float>() { 60, 70, 80, 0};
+    public float current;
     private GameObject player;
     //NEED TO ADD COUNTDOWN TIMER
     //LOSE A LIFE EVERYTIME TIME IS ZERO
@@ -34,11 +36,18 @@ public class Levels : MonoBehaviour
         lose.SetActive(false);
         losing = GetComponent<Text>();
         player = GameObject.Find("Player Sprite");
+        current = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!(level == 4))
+        {
+        current -= Time.deltaTime;
+        }
+        timeText.text = "Timer: " + (int)current +"s";
+
         Ammo.text = "Ammo: " + controller.ammo;
 
         if (controller.ammo == 0 || life == 0)
@@ -47,46 +56,40 @@ public class Levels : MonoBehaviour
             losing.text = "You Lose!" + "\n" + "\n" + "Level "+ level + "   Enemies Left : " + controller.enemiesLeft + "\n" + "\n" + "Press 'R' to Restart";
             //stop timer
         }
-        if (time == 0)
+        if (current < 0)
         {
             life -= 1;
+            lives[life].SetActive(false);
+            current = timeleft[level-1];
         }
-        if (controller.enemiesLeft == 0)
+        if (controller.enemiesLeft == 0 && !(level == 4))
         {
             controller.ammo = 15;
-
-            if (level == 3)
-            {
-                winning.text = "You Win!";
-                win.SetActive(true);
-                //stop timer
-            }
             level += 1;
-        }
+            current = timeleft[level - 1];
+
         if (level == 2)
         {
             transform.position = new Vector3(0, 12,-10);
             player.transform.position = new Vector3(0, 12, player.transform.position.z);
             controller.enemiesLeft = 7;
-            time = 150;
         }
         if (level == 3)
         {
             transform.position = new Vector3(0, 24,-10);
             player.transform.position = new Vector3(0, 23, player.transform.position.z);
             controller.enemiesLeft = 10;
-            time = 180;
         }
-
+            if (level == 4)
+            {
+                winning.text = "You Win!";
+                win.SetActive(true);
+                //stop timer
+            }
+        }
     }
     //void life()
     //{
-    //    if (life == 3)
-    //    {
-    //        life3.SetActive(true);
-    //        life2.SetActive(true);
-    //        life1.SetActive(true);
-    //    }
     //    if (life == 2)
     //    {
     //        life3.SetActive(false);
